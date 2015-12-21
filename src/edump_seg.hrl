4144310b3ab76e1593f5ed51eced2c6fc1282551
Geoff Cant <nem@erlang.geek.nz>
2015-12-21 18:05:44 +1300

    #pos.seg_start starts as unknown now.

--- src/edump_seg.hrl
+++ src/edump_seg.hrl
@@ -1,4 +1,4 @@
 -record(seg, {id :: binary(),
-              seg_start :: pos_integer(),
+              seg_start = unknown :: pos_integer() | unknown,
               data_start = unknown :: pos_integer() | undefined | unknown,
               seg_end = unknown :: pos_integer() | unknown}).
