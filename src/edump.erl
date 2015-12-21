1cad0c52938a08d49fba1cf9c6e752158950331b
Geoff Cant <nem@erlang.geek.nz>
2015-12-21 15:39:28 +1300

    Begin implementing API module.

--- src/edump.erl
+++ src/edump.erl
@@ -1,12 +1,37 @@
 -module('edump').
 
 %% API exports
--export([]).
+-export([open/1
+        ,open/2
+        ,reopen/1
+        ,proc_graph/1
+        ,try_parse/1
+        ,try_parse/2
+        ]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
+open(CrashdumpFile) ->
+    edump_idx:open(CrashdumpFile).
+
+open(CrashdumpFile, Opts) ->
+    edump_idx:open(CrashdumpFile, Opts).
+
+reopen(Handle) ->
+    edump_idx:reopen(Handle).
+
+proc_graph(Handle) ->
+    edump_proc_graph:from_handle(Handle).
+
+try_parse(Dump) ->
+    try_parse(any, Dump).
+
+try_parse(Type, Dump) when is_list(Dump) ->
+    try_parse(Type, open(Dump));
+try_parse(Type, Dump) ->
+    edump_seg:first_parse_failure(Type, Dump).
 
 %%====================================================================
 %% Internal functions
